b1683071fc1cca85bce1e3fcc50098812bd43f60
Daniel Schlick <d.schlick@outlook.com>
2022-01-12 17:56:29 +0100

    initial project setup

---
+++ src/Lanternfish.Logic/Model/Lanternfish.cs
@@ -0,0 +1,12 @@
+﻿namespace Lanternfish.Logic;
+public class Lanternfish
+{
+    public int Age { get; set; }
+    public int ReproductionTime { get; set; }
+
+    public Lanternfish(int reproductionTime)
+    {
+        Age = 0;
+        ReproductionTime = reproductionTime;
+    }
+}
